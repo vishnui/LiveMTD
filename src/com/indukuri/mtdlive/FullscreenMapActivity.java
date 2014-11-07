6025df01845ba178aa46880919713d95110221a8
Vishnu Indukuri <vishnui@gmail.com>
2014-11-06 21:31:32 -0600

    first working version

--- src/com/indukuri/mtdlive/FullscreenMapActivity.java
+++ src/com/indukuri/mtdlive/FullscreenMapActivity.java
@@ -1,24 +1,17 @@
 package com.indukuri.mtdlive;
 
-import java.util.concurrent.Semaphore;
-
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Window;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.indukuri.mtdlive.util.SystemUiHider;
 
@@ -28,89 +21,20 @@ import com.indukuri.mtdlive.util.SystemUiHider;
  * 
  * @see SystemUiHider
  */
-public class FullscreenMapActivity extends Activity {
+public class FullscreenMapActivity extends FragmentActivity {
 
-	private static final boolean AUTO_HIDE = true;
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-	private static final boolean TOGGLE_ON_CLICK = true;
-	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-	private SystemUiHider mSystemUiHider;
-	private MapView contentView;
+	private SupportMapFragment mapView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_fullscreen_map);
-
-		setupLocationTracking();
-		startMTDDataPolling();
-		
-		final View controlsView = findViewById(R.id.fullscreen_content_controls);
-		contentView = (MapView) findViewById(R.id.fullscreen_content);
-
-		// Set up an instance of SystemUiHider to control the system UI for
-		// this activity.
-		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
-				HIDER_FLAGS);
-		mSystemUiHider.setup();
-		mSystemUiHider
-				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-					// Cached values.
-					int mControlsHeight;
-					int mShortAnimTime;
-
-					@Override
-					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-					public void onVisibilityChange(boolean visible) {
-						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-							// If the ViewPropertyAnimator API is available
-							// (Honeycomb MR2 and later), use it to animate the
-							// in-layout UI controls at the bottom of the
-							// screen.
-							if (mControlsHeight == 0) {
-								mControlsHeight = controlsView.getHeight();
-							}
-							if (mShortAnimTime == 0) {
-								mShortAnimTime = getResources().getInteger(
-										android.R.integer.config_shortAnimTime);
-							}
-							controlsView
-									.animate()
-									.translationY(visible ? 0 : mControlsHeight)
-									.setDuration(mShortAnimTime);
-						} else {
-							// If the ViewPropertyAnimator APIs aren't
-							// available, simply show or hide the in-layout UI
-							// controls.
-							controlsView.setVisibility(visible ? View.VISIBLE
-									: View.GONE);
-						}
-
-						if (visible && AUTO_HIDE) {
-							// Schedule a hide().
-							delayedHide(AUTO_HIDE_DELAY_MILLIS);
-						}
-					}
-				});
-
-		// Set up the user interaction to manually show or hide the system UI.
-		contentView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (TOGGLE_ON_CLICK) {
-					mSystemUiHider.toggle();
-				} else {
-					mSystemUiHider.show();
-				}
-			}
-		});
-
-		// Upon interacting with UI controls, delay any scheduled hide()
-		// operations to prevent the jarring behavior of controls going away
-		// while interacting with the UI.
-		findViewById(R.id.dummy_button).setOnTouchListener(
-				mDelayHideTouchListener);
+		mapView = SupportMapFragment.newInstance();
+		FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+				.beginTransaction();
+		fragmentTransaction.add(R.id.container, mapView);
+		fragmentTransaction.commit();
 	}
 
 	// Location Tracking Setup
@@ -121,71 +45,23 @@ public class FullscreenMapActivity extends Activity {
 
 	public void startMTDDataPolling() {
 		locations = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		Location loc = locations
-				.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-		map = contentView.getMap();
-		mtdUpdates = new MTDThread(loc, map);
+		map = mapView.getMap();
+		map.getUiSettings().setMyLocationButtonEnabled(true);
+		map.setMyLocationEnabled(true);
+		MapsInitializer.initialize(this);
+		mtdUpdates = new MTDThread(map, this);
 		mtdUpdates.start();
 	}
 
-	public void setupLocationTracking() {
-		locations.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 5,
-				new LocationListener() {
-					public void onStatusChanged(String provider, int status,
-							Bundle extras) {					}
-					public void onProviderEnabled(String provider) {					}
-					public void onProviderDisabled(String provider) {					}
-					public void onLocationChanged(Location location) {
-						map.animateCamera(CameraUpdateFactory
-								.newLatLng(new LatLng(location.getLatitude(),
-										location.getLongitude())));
-						// TODO Move Icon
-					}
-				});
-	}
-
 	// ----------------------------------------------------------------------------------
 
-	// FULL SCREEN RELATED SHIT
-	// ----------------------------------------------------------------------------------
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(100);
+		startMTDDataPolling();
+		Location loc = locations
+				.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		LatLng pos = new LatLng(loc.getLatitude(), loc.getLongitude());
+		map.animateCamera(CameraUpdateFactory.newLatLngZoom(pos, 12));
 	}
-
-	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
-	 */
-	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				delayedHide(AUTO_HIDE_DELAY_MILLIS);
-			}
-			return false;
-		}
-	};
-	Handler mHideHandler = new Handler();
-	Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			mSystemUiHider.hide();
-		}
-	};
-
-	/**
-	 * Schedules a call to hide() in [delay] milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
-	}
-	// -----------------------------------------------------------------------------------
 }
--- src/com/indukuri/mtdlive/MTDThread.java
+++ src/com/indukuri/mtdlive/MTDThread.java
@@ -6,28 +6,33 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Locale;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.location.Location;
+import android.app.Activity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MTDThread extends Thread {
+	private static final long MINUTE= 30*1000 ;
 	private boolean running;
 	private GoogleMap map;
-	private String reqString = "https://developer.cumtd.com/api/v2.2/json/GetVehicles?key=712bc9d438744869ab22ac4557ebd01b";
-
-	public MTDThread(Location l, GoogleMap mv) {
+	private String reqString = "https://developer.cumtd.com/api/v2.2/json/GetVehicles" +
+			"?key=712bc9d438744869ab22ac4557ebd01b";
+	private Activity context ;
+	private JSONObject resp  ;
+	
+	public MTDThread(GoogleMap mv, Activity c) {
 		running = true;
 		map = mv;
+		context = c ;
 	}
 
 	@Override
@@ -40,8 +45,8 @@ public class MTDThread extends Thread {
 						.openConnection();
 				BufferedReader in = new BufferedReader(new InputStreamReader(
 						urlConnection.getInputStream()));
-				JSONObject response = readStream(in);
-				processResponse(response);
+				resp = readStream(in);
+				context.runOnUiThread(runner) ;
 			} catch (MalformedURLException e1) {
 				e1.printStackTrace();
 			} catch (IOException e) {
@@ -49,14 +54,24 @@ public class MTDThread extends Thread {
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-			// Sleep for a full second before asking for an update
+			// Sleep for a full minute before asking for an update
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(MINUTE);
 			} catch (InterruptedException e) {
 			}
 		}
 	}
-
+	
+	private Runnable runner = new Runnable() {
+		public void run() {
+			try {
+				processResponse(resp);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+	};
+	
 	public void processResponse(JSONObject resp) throws JSONException{
 		JSONArray vehicles = resp.getJSONArray("vehicles");
 		int numVehicles = vehicles.length() ;
@@ -95,7 +110,7 @@ public class MTDThread extends Thread {
 	}
 	
 	public BitmapDescriptor getIcon(String color){
-		color = color.toLowerCase();
+		color = color.toLowerCase(Locale.US);
 		// TODO ALL the diff colors
 		return BitmapDescriptorFactory.fromResource(R.drawable.greenbus) ;
 	}
