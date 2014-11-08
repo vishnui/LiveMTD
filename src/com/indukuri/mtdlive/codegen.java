245d12ce5832174c29cbe644a0de85851041fc5a
Vishnu Indukuri <vishnui@gmail.com>
2014-11-08 15:13:14 -0600

    codegen for buses

---
+++ src/com/indukuri/mtdlive/codegen.java
@@ -0,0 +1,22 @@
+package com.indukuri.mtdlive ;
+
+import java.io.File;
+
+public class codegen{
+    public static void main(String[] args){
+        File folder = new File("./");
+		File[] listOfFiles = folder.listFiles();
+		// Read each file in the directory
+		for (File file : listOfFiles) {
+			if (file.isFile()) {
+			    String name = file.getName() ;
+			    int buspngIndex = name.indexOf("bus.png") ;
+			    if(buspngIndex == -1) continue ;
+			    name = name.substring(0, buspngIndex) ;
+			    String line ;
+			    line = "else if(route.contains(\""+name+"\")) ret = R.drawable."+name+"bus ;" ;
+				System.out.println(line) ;
+			}
+		}
+	}
+}
